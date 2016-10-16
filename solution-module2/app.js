424d73d44480bfb0156ea21224cd2ab1801682e5
Jason Kan <aaa@aaa.com>
2016-10-16 15:03:26 -0400

    .

---
+++ solution-module2/app.js
@@ -0,0 +1,52 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+  toBuy.buyItems = function (itemIndex) {
+    ShoppingListCheckOffService.buyItems(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var toBuyItems = [{"name": "cookies", "quantity": "100"},
+                    {"name": "popcorn", "quantity": "20 bags of"},
+                    {"name": "apple juice", "quantity": "100 cans of"},
+                    {"name": "plates", "quantity": "100"},
+                    {"name": "candies", "quantity": "25"}];
+  var boughtItems = [];
+
+  service.buyItems = function (itemIndex) {
+    boughtItems.push(toBuyItems[itemIndex]);
+    toBuyItems.splice(itemIndex, 1);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
